dd5fdc1d3cd10a1a6a6206d05326eadf76884b4e
Alex Reichert <alexreichert621@gmail.com>
2014-05-15 19:06:41 -0700

    formatting

--- javascript/exponent.js
+++ javascript/exponent.js
@@ -4,15 +4,15 @@ function exp(base, n) {
 }
 
 function exp2(base, n) {
-  if (n === 0) { return 1 }
+	if (n === 0) { return 1 }
 	var num;
-  if (n % 2 === 0) {
-    num = exp2(base, n/2);
-    return num * num;
-  } else {
-    num = exp2(base, (n - 1) / 2);
-    return base * num * num;
-  }
+	if (n % 2 === 0) {
+		num = exp2(base, n/2);
+		return num * num;
+	} else {
+		num = exp2(base, (n - 1) / 2);
+		return base * num * num;
+	}
 }
 
 // console.log(exp(3, 8));
