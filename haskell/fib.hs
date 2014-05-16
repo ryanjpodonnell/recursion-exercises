d872584618aa78538ba3a55a29b6b35dfdae1bd3
Alex Reichert <alexreichert621@gmail.com>
2014-05-16 00:09:36 -0700

    fix second fib method

--- haskell/fib.hs
+++ haskell/fib.hs
@@ -8,6 +8,6 @@ fibList n = map fib [1..n]
 
 -- combined version...
 fib'::Int->[Int]
-fib' 0 = [0]
-fib' 1 = [0, 1]
-fib' n = fib' (n-1) ++ [last (fib' (n-1)) + last (fib' (n-2))]
+fib' 1 = [0]
+fib' 2 = [0, 1]
+fib' n = fib' (n-1) ++ [(last (fib' (n-1)) + last (fib' (n-2)))]
