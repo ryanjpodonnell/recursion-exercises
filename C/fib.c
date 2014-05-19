69aa86a473b116a4dc045c3a0a1b47b3208ec8e9
Weyman Fung <wfung0083@yahoo.com>
2014-05-19 16:59:16 -0700

    bsearch and fibs

--- C/fib.c
+++ C/fib.c
@@ -1,14 +1,14 @@
-int *fibs(int n) {
-	if ( n <= 0 )
-		return // int * ar = []
-
-
-
-
-
+unsigned int fibonacci_recursive(unsigned int n)
+{
+    if (n == 0) 
+    {
+        return 0;
+     } 
+     if (n == 1) {
+           return 1;
+     }
+     return fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2);
 }
-
-
 
 
 
