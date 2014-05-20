798e70e0935c4207b075477f922a34a8ab6fd00f
Alex Reichert <alexreichert621@gmail.com>
2014-05-19 17:15:55 -0700

    c formatting

--- C/fib.c
+++ C/fib.c
@@ -1,13 +1,13 @@
 unsigned int fibonacci_recursive(unsigned int n)
 {
-    if (n == 0) 
-    {
-        return 0;
-     } 
-     if (n == 1) {
-           return 1;
-     }
-     return fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2);
+	if (n == 0) 
+	{
+		return 0;
+	} 
+	if (n == 1) {
+		return 1;
+	}
+	return fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2);
 
 }
 
