ba84a292d6d5167b5e3dd4d680b377311d6dc7e7
Alex Reichert <alexreichert621@gmail.com>
2014-05-20 11:38:04 -0700

    add python recursive sum and js mergesort

--- python/warmup.py
+++ python/warmup.py
@@ -5,3 +5,13 @@ def recRange(first, last):
         return [first] + recRange(first + 1, last)
         
 # print recRange(-4,10)
+
+def sum(arr):
+    if len(arr) == 0:
+        return 0
+    elif len(arr) == 1:
+        return arr[0]
+    else:
+        return arr.pop() + sum(arr)
+        
+# print sum([5,6,7,8])
